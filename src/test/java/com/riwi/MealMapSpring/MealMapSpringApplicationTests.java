515e494b090a871ac37228d37e29ebaa28872ac8
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-09-06 17:00:04 -0500

    Second commit

---
+++ src/test/java/com/riwi/MealMapSpring/MealMapSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.riwi.MealMapSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MealMapSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
