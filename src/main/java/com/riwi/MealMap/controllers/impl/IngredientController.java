dd6d44c93bb1c96cfeb2090b6296db9186b1660f
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-09-12 16:30:00 -0500

    Repair errors in develop

--- src/main/java/com/riwi/MealMap/controllers/impl/IngredientController.java
+++ src/main/java/com/riwi/MealMap/controllers/impl/IngredientController.java
@@ -44,11 +44,6 @@ public class IngredientController implements IIngredientController {
     @Override
     @GetMapping("/readById/{id}")
     public Optional<Ingredient> readById(@PathVariable Integer id) {
-        String url = "http://localhost:3000/orders";
-        String response = restTemplate.getForObject(url, String.class);
-
-        System.out.println(response);
-
         return ingredientService.readById(id);
     }
 
