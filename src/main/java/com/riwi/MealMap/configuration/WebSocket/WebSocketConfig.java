34bef6fefe5136002faf3ad947de207339a87477
samuelcoder1 <echeverrysamuel74@gmail.com>
2024-09-13 16:09:54 -0400

    Changes in WebSocket config

--- src/main/java/com/riwi/MealMap/configuration/WebSocket/WebSocketConfig.java
+++ src/main/java/com/riwi/MealMap/configuration/WebSocket/WebSocketConfig.java
@@ -1,20 +1,23 @@
 package com.riwi.MealMap.configuration.WebSocket;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.socket.config.annotation.EnableWebSocket;
-import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
-import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.*;
 
 @Configuration
-@EnableWebSocket
-public class WebSocketConfig implements WebSocketConfigurer {
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
-    public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
-        registry.addHandler(myHandler(), "/ws/myHandler")
-                .setAllowedOrigins("*");
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        // Configura el prefijo del destino para los mensajes de cliente a servidor
+        config.setApplicationDestinationPrefixes("/app");
+        // Configura el prefijo para los destinos de mensajes del servidor al cliente
+        config.enableSimpleBroker("/topic", "/queue");
     }
 
-    public MyWebSocketHandler myHandler() {
-        return new MyWebSocketHandler();
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        // Configura el endpoint de WebSocket
+        registry.addEndpoint("/ws").setAllowedOrigins("*").withSockJS();
     }
 }
